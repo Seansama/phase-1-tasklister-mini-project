b9d9feff5dca647843d503ad5106d9269f942750
ShaunM <seansama__@pop-os.localdomain>
2022-08-26 13:22:43 +0300

    Done with Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const getElementById = (id) => document.getElementById(id);
+  const addToListHandler = (event) => {
+    event.preventDefault();
+    const input = getElementById('new-task-description');
+    const list = getElementById('tasks');
+    if(input.value.length === 0){
+      return;
+    }
+    const newList = document.createElement('li');
+    newList.innerHTML = input.value;
+    list.appendChild(newList)
+    input.value = '';
+  }
+  const form = getElementById('create-task-form');
+  form.addEventListener('submit', addToListHandler)
 });
